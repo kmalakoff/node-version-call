23b6daeceaa4712c27be44a0a3557aa414161b4c
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-10 12:38:18 -0700

    Installed nvu if it is missing

--- src/worker.ts
+++ src/worker.ts
@@ -1,11 +1,17 @@
 const fs = require('fs');
 const JSONBuffer = require('json-buffer');
+const assign = require('just-extend');
 const callFn = require('./callFn');
 
 // get data
 const input = process.argv[2];
 const output = process.argv[3];
 const callData = JSONBuffer.parse(fs.readFileSync(input, 'utf8'));
+
+// set up env
+if (process.cwd() !== callData.cwd) process.chdir(callData.cwd);
+for (const key in process.env) delete process.env[key];
+assign(process.env, callData.env);
 
 // call function
 const result = callFn(callData.filePath, callData.args);
