2a02d910f15656306b84ac8fd4342d1d5a4e5ceb
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-19 08:36:15 -0700

    Added callbacks

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,31 @@ const functionExec = require('function-exec-sync');
 const versionExecPath = require('./versionExecPath.ts');
 const SLEEP_MS = 60;
 
-export default function call(version: string, filePath: string /* arguments */): any {
+export type VersionInfo = {
+  version: string;
+  callbacks: boolean;
+};
+
+export default function call(version: string | VersionInfo, filePath: string /* arguments */): any {
   const args = Array.prototype.slice.call(arguments, 2);
+  let callbacks = false;
+  if (typeof version !== 'string') {
+    callbacks = (version as VersionInfo).callbacks ?? false;
+    version = (version as VersionInfo).version;
+
+    // need to unwrap callbacks
+    if (callbacks && version === 'local') version = process.version;
+  }
 
   // local - just call
-  if (version === 'local') {
+  if (version === 'local' && !callbacks) {
     const fn = require(filePath);
     return typeof fn == 'function' ? fn.apply(null, args) : fn;
-  } 
+  }
+
   // call a version of node
   else {
     const execPath = versionExecPath(version);
-    return functionExec({execPath, env: process.env, cwd: process.cwd(), sleep: SLEEP_MS}, filePath, ...args)  
+    return functionExec.apply(null, [{ execPath, env: process.env, cwd: process.cwd(), sleep: SLEEP_MS, callbacks }, filePath].concat(args));
   }
 }
