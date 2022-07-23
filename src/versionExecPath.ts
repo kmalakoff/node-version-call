d71cfb151f5016f6ace350778bdd1d9d87003dde
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-23 14:02:42 -0700

    Replaced node-version-use with node-install-release

--- src/versionExecPath.ts
+++ src/versionExecPath.ts
@@ -1,27 +1,27 @@
 const path = require('path');
 const accessSync = require('fs-access-sync-compat');
 const constants = require('./constants');
-let resolveVersion = null; // break dependencies
 
+const SLEEP_MS = 200;
+const installVersion = __dirname + '/installVersion.js';
+
+let resolveVersion = null; // break dependencies
 export default function versionExecPath(version) {
   if (!resolveVersion) resolveVersion = require('node-resolve-versions'); // break dependencies
 
-  const versions = resolveVersion.sync(version);
+  const versions = require('node-resolve-versions').sync(version);
   if (versions.length > 1) throw new Error('Multiple versions match: ' + version + ' = ' + versions.join(',') + '. Please be specific');
   const installPath = path.join(constants.installDirectory, versions[0]);
   const binRoot = constants.isWindows ? installPath : path.join(installPath, 'bin');
   const execPath = path.join(binRoot, constants.node);
 
   try {
-    // ensure installed
-    accessSync(installPath);
+    // check installed
+    accessSync(execPath);
   } catch (err) {
     // need to install
-    try {
-      require('cross-spawn-cb').sync('nvu', [versions[0], constants.node, '--version'], { encoding: 'utf8' });
-    } catch (err) {
-      if (!err.stderr || err.stderr.indexOf('ExperimentalWarning') < 0) throw err;
-    }
+    require('function-exec-sync')({ cwd: process.cwd(), sleep: SLEEP_MS, callbacks: true }, installVersion, versions[0]);
+    accessSync(execPath);
   }
   return execPath;
 }
--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -17,7 +17,7 @@ describe('node-version-call', function () {
   describe('callbacks', function () {
     addTests(function (version) {
       return function () {
-        this.timeout(20000);
+        this.timeout(200000);
         const fnPath = path.join(DATA, 'callbacks.js');
         const result = call({ version, callbacks: true }, fnPath, 'arg1');
         assert.equal(result, 'arg1');
@@ -28,7 +28,7 @@ describe('node-version-call', function () {
   describe('no export', function () {
     addTests(function (version) {
       return function () {
-        this.timeout(20000);
+        this.timeout(200000);
         const fnPath = path.join(DATA, 'noExport.js');
         const result = call(version, fnPath);
         assert.equal(keys(result).length, 0);
@@ -39,7 +39,7 @@ describe('node-version-call', function () {
   describe('process version', function () {
     addTests(function (version) {
       return function () {
-        this.timeout(20000);
+        this.timeout(200000);
         const fnPath = path.join(DATA, 'processVersion.js');
         const result = call(version, fnPath);
         if (version === 'local') assert.equal(result, process.version);
@@ -54,7 +54,7 @@ describe('node-version-call', function () {
   describe('return arguments', function () {
     addTests(function (version) {
       return function () {
-        this.timeout(20000);
+        this.timeout(200000);
         const args = [
           { field2: 1 },
           1,
@@ -75,7 +75,7 @@ describe('node-version-call', function () {
   describe('throw error', function () {
     addTests(function (version) {
       return function () {
-        this.timeout(20000);
+        this.timeout(200000);
         const fnPath = path.join(DATA, 'throwError.js');
         try {
           call(version, fnPath);
