f8cde7d8cb64dcc17436b387f2e84e43c264a243
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-23 14:02:42 -0700

    Replaced node-version-use with node-install-release

--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,6 @@ module.exports = {
   isWindows: process.platform === 'win32',
   node: process.platform === 'win32' ? 'node.exe' : 'node',
   cacheDirectory: path.join(home, '.nvu', 'cache'),
+  buildDirectory: path.join(home, '.nvu', 'build'),
   installDirectory: path.join(home, '.nvu', 'installed'),
 };
