5aa66410128517a8589c4b70c8dd9188039a0238
Kevin Malakoff <kmalakoff@gmail.com>
2022-08-04 18:22:22 -0700

    Added debug code

--- src/installVersion.ts
+++ src/installVersion.ts
@@ -1,8 +1,20 @@
-const path = require('path');
-const installRelease = require('node-install-release');
-const constants = require('./constants');
+let path = null;
+let installRelease = null;
+let constants = null;
+let err = null;
+
+// TODO: remove debug code
+try {
+  path = require('path');
+  installRelease = require('node-install-release');
+  constants = require('./constants');
+} catch (_err) {
+  err = err;
+}
 
 export default function installVersion(version: string, callback: (error?: Error) => void): void {
+  if (err) return callback(new Error('Failed to load modules' + err.message));
+
   const installPath = path.join(constants.installDirectory, version);
   installRelease(version, installPath, { cacheDirectory: constants.cacheDirectory, buildDirectory: constants.buildDirectory }, callback);
 }
