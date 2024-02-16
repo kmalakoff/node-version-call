64e1278d653fc35f761590e52e80ac8cd67bfa78
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-15 17:22:44 -0800

    Upgrade to ts-dev-stack

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-Object.defineProperty(exports, "default", {
+Object.defineProperty(exports, // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+"default", {
     enumerable: true,
     get: function() {
         return call;
@@ -11,13 +12,15 @@ Object.defineProperty(exports, "default", {
 var versionExecPath = require("./versionExecPath.js");
 var SLEEP_MS = 60;
 var functionExec = null; // break dependencies
-function call(version, filePath /* arguments */ ) {
+function call(version, filePath) {
+    for(var _len = arguments.length, args = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++){
+        args[_key - 2] = arguments[_key];
+    }
     if (!functionExec) functionExec = require("function-exec-sync"); // break dependencies
-    var args = Array.prototype.slice.call(arguments, 2);
     var callbacks = false;
     if (typeof version !== "string") {
-        var _callbacks;
-        callbacks = (_callbacks = version.callbacks) !== null && _callbacks !== void 0 ? _callbacks : false;
+        var _version_callbacks;
+        callbacks = (_version_callbacks = version.callbacks) !== null && _version_callbacks !== void 0 ? _version_callbacks : false;
         version = version.version;
         // need to unwrap callbacks
         if (callbacks && version === "local") version = process.version;
@@ -25,20 +28,20 @@ function call(version, filePath /* arguments */ ) {
     // local - just call
     if (version === "local" && !callbacks) {
         var fn = require(filePath);
-        return typeof fn == "function" ? fn.apply(null, args) : fn;
-    } else {
-        var execPath = versionExecPath(version);
-        return functionExec.apply(null, [
-            {
-                execPath: execPath,
-                env: process.env,
-                cwd: process.cwd(),
-                sleep: SLEEP_MS,
-                callbacks: callbacks
-            },
-            filePath
-        ].concat(args));
+        return typeof fn === "function" ? fn.apply(null, args) : fn;
     }
+    // call a version of node
+    var execPath = versionExecPath(version);
+    return functionExec.apply(null, [
+        {
+            execPath: execPath,
+            env: process.env,
+            cwd: process.cwd(),
+            sleep: SLEEP_MS,
+            callbacks: callbacks
+        },
+        filePath
+    ].concat(args));
 }
 
 if ((typeof exports.default === 'function' || (typeof exports.default === 'object' && exports.default !== null)) && typeof exports.default.__esModule === 'undefined') {
