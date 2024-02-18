1d9e14fcbdffc294257255c07415ae2dac6b67d3
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-18 10:20:52 -0800

    Upgrade modules

--- src/installVersion.ts
+++ src/installVersion.ts
@@ -4,5 +4,13 @@ const constants = require('./constants');
 
 export default function installVersion(version: string, callback: (error?: Error) => void): void {
   const installPath = path.join(constants.installDirectory, version);
-  installRelease(version, installPath, { cacheDirectory: constants.cacheDirectory, buildDirectory: constants.buildDirectory }, callback);
+  installRelease(
+    version,
+    installPath,
+    {
+      cacheDirectory: constants.cacheDirectory,
+      buildDirectory: constants.buildDirectory,
+    },
+    callback
+  );
 }
