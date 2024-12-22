14d130472e785d65cd49f12b913bc2b87c34de26
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-21 20:10:25 -0800

    Refactor for testability

--- src/workers/installVersion.ts
+++ src/workers/installVersion.ts
@@ -1,11 +1,12 @@
 import path from 'path';
-import { buildDirectory, cacheDirectory, installDirectory } from '../constants.js';
 // @ts-ignore
 import lazy from '../lib/lazy.cjs';
+import type { InstallDirs } from '../types.js';
 
 const installRelease = lazy('node-install-release');
 
-export default function installVersion(version: string, callback: (error?: Error) => void): void {
+export default function installVersion(version: string, installDirs: InstallDirs, callback: (error?: Error) => void): void {
+  const { buildDirectory, cacheDirectory, installDirectory } = installDirs;
   const installPath = path.join(installDirectory, version);
   installRelease()(version, installPath, { cacheDirectory, buildDirectory }, callback);
 }
