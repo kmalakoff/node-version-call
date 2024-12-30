18ba0a9df5014828836f624b49ca7c3cdb9f9d5c
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-29 22:58:28 -0800

    refactor: use node-version-install for version resolution

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE);
-export const node = isWindows ? 'node.exe' : 'node';
+export const NODE = isWindows ? 'node.exe' : 'node';
