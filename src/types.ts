37fa83cb320f0d78314274cd12b30a8783a36fda
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 00:19:05 -0800

    Added wrap helper

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,13 @@ export type VersionInfo = {
   storagePath: string;
   env?: NodeJS.ProcessEnv;
 };
+
+export type WrapOptions = {
+  callbacks?: boolean;
+  storagePath?: string;
+  env?: NodeJS.ProcessEnv;
+};
+
+export type WrapperCallback = (err: unknown, result?: unknown) => void;
+
+export type Wrapper = (version: string, ...args: unknown[]) => void;
