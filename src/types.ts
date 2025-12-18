948b43ef1917d28c33fca3296f4358d66d3162c7
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 22:07:11 -0800

    Add bind and call

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,33 @@
-export type VersionInfo = {
-  version: string;
+export type CallOptions = {
   callbacks?: boolean;
-  storagePath: string;
+  storagePath?: string;
   env?: NodeJS.ProcessEnv;
 };
 
+export type BindOptions = CallOptions;
+
+export type CallerCallback = (err: unknown, result?: unknown) => void;
+
+export type BoundCaller = (...args: unknown[]) => unknown;
+
+// Deprecated types - kept for backwards compatibility
+/** @deprecated Use CallOptions instead */
+export type VersionInfo = {
+  version: string;
+  callbacks?: boolean;
+  storagePath?: string;
+  env?: NodeJS.ProcessEnv;
+};
+
+/** @deprecated Use BindOptions instead */
 export type WrapOptions = {
   callbacks?: boolean;
   storagePath?: string;
   env?: NodeJS.ProcessEnv;
 };
 
+/** @deprecated Use CallerCallback instead */
 export type WrapperCallback = (err: unknown, result?: unknown) => void;
 
+/** @deprecated Use BoundCaller instead */
 export type Wrapper = (version: string, ...args: unknown[]) => void;
